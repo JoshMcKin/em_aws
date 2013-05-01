081d9939ed63bfcf4b5578409e7733f1f8b00225
Joshua Mckinney <joshmckin@gmail.com>
2013-05-01 13:00:14 -0500

    set HotTub logger

--- lib/em_aws.rb
+++ lib/em_aws.rb
@@ -5,6 +5,8 @@ require 'em-http'
 require 'em-synchrony'
 require 'em-synchrony/em-http'
 require 'aws/core/http/em_http_handler'
+require 'hot_tub'
 
 AWS.eager_autoload! # lazy load isn't thread safe
+HotTub.logger = AWS.config.logger
 module EmAws;end
