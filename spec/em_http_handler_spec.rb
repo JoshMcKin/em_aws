666d568b84801a7ab74f39cb477c53305d17c3b9
Kyle Bishop <kybishop@gmail.com>
2013-06-17 19:52:43 -0400

    cleaned up the specs

--- spec/em_http_handler_spec.rb
+++ spec/em_http_handler_spec.rb
@@ -22,7 +22,7 @@ module AWS::Core
       end
     end
 
-    # A slow server for testing timeout, 
+    # A slow server for testing timeout,
     # borrowed from: http://www.igvita.com/2008/05/27/ruby-eventmachine-the-speed-demon/
     class SlowServer < EventMachine::Connection
       include EventMachine::HttpServer
@@ -39,10 +39,9 @@ module AWS::Core
     end
 
     describe EMHttpHandler do
-
       let(:handler) { EMHttpHandler.new(default_request_options) }
 
-      let(:default_request_options) { {} }
+      let(:default_request_options) { Hash.new }
 
       let(:req) do
         r = Http::Request.new
@@ -56,50 +55,112 @@ module AWS::Core
 
       let(:em_http_options) do
         options = nil
-        EMHttpHandler.should_receive(:fetch_response).with do |url, method,opts|
+        EMHttpHandler.should_receive(:fetch_response).with do |url, _, opts|
           options = opts
           double("http response",
                  :response => "<foo/>",
                  :code => 200,
                  :to_hash => {})
         end
+
         handler.handle(req, resp)
+
         options
       end
 
       it 'should be accessible from AWS as well as AWS::Core' do
-        AWS::Http::EMHttpHandler.new.should
-        be_an(AWS::Core::Http::EMHttpHandler)
+        AWS::Http::EMHttpHandler.new
+          .should be_an(AWS::Core::Http::EMHttpHandler)
+      end
+
+      it "should not timeout" do
+        EM.synchrony do
+          response = Http::Response.new
+          request = Http::Request.new
+          request.host = "127.0.0.1"
+          request.port = "8081"
+          request.uri = "/"
+          request.body_stream = StringIO.new("myStringIO")
+
+          # turn on our test server
+          EventMachine::run do
+            EventMachine::start_server request.host, request.port, SlowServer
+          end
+
+          handler.stub(:fetch_url).and_return("http://127.0.0.1:8081")
+
+          handler.handle(request,response)
+
+          response.network_error.should be_nil
+
+          EM.stop
+        end
+      end
+
+      it "should timeout after 0.1 seconds" do
+        EM.synchrony do
+          response = Http::Response.new
+          request = Http::Request.new
+          request.host = "127.0.0.1"
+          request.port = "8081"
+          request.uri = "/"
+          request.body_stream = StringIO.new("myStringIO")
+
+          # turn on our test server
+          EventMachine::run do
+            EventMachine::start_server request.host, request.port, SlowServer
+          end
+
+          handler.stub(:fetch_url).and_return("http://127.0.0.1:8081")
+          request.stub(:read_timeout).and_return(0.01)
+          handler.stub(:connect_timeout).and_return(1) #just to speed up the test
+
+          handler.handle(request,response)
+
+          response.network_error.should be_a(Timeout::Error)
+
+          EM.stop
+        end
       end
 
       describe '#handle' do
         context 'timeouts' do
           it 'should rescue Timeout::Error' do
-            handler.stub(:fetch_response).
-              and_raise(Timeout::Error)
-            lambda { handler.handle(req, resp) }.
-              should_not raise_error
+            handler
+              .stub(:fetch_response)
+              .and_raise(Timeout::Error)
+
+            expect {
+              handler.handle(req, resp)
+            }.to_not raise_error
           end
 
           it 'should rescue Errno::ETIMEDOUT' do
-            handler.stub(:fetch_response).
-              and_raise(Errno::ETIMEDOUT)
-            lambda { handler.handle(req, resp) }.
-              should_not raise_error
+            handler
+              .stub(:fetch_response)
+              .and_raise(Errno::ETIMEDOUT)
+
+            expect {
+              handler.handle(req, resp)
+            }.to_not raise_error
           end
 
           it 'should indicate that there was a network_error' do
-            handler.stub(:fetch_response).
-              and_raise(Errno::ETIMEDOUT)
+            handler
+              .stub(:fetch_response)
+              .and_raise(Errno::ETIMEDOUT)
+
             handler.handle(req, resp)
+
             resp.network_error?.should be_true
           end
         end
 
         context 'default request options' do
           before(:each) do
-            handler.stub(:default_request_options).and_return({ :foo => "BAR",
-                                                                :private_key_file => "blarg" })
+            handler
+              .stub(:default_request_options)
+              .and_return(:foo => "BAR", :private_key_file => "blarg")
           end
 
           it 'passes extra options through to synchrony' do
@@ -115,91 +176,74 @@ module AWS::Core
 
       describe '#fetch_request_options' do
         it "should set :query and :body to request.querystring" do
-          opts = handler.send(:fetch_request_options,(req))
+          opts = handler.send(:fetch_request_options, req)
           opts[:query].should eql(req.querystring)
         end
 
         it "should set :path to request.path" do
-          opts = handler.send(:fetch_request_options,(req))
+          opts = handler.send(:fetch_request_options, req)
           opts[:path].should eql(req.path)
         end
+
         context "request.body_stream is a StringIO" do
           it "should set :body to request.body_stream" do
-            opts = handler.send(:fetch_request_options,(req))
+            opts = handler.send(:fetch_request_options, req)
             opts[:body].should eql("myStringIO")
           end
         end
+
         context "request.body_stream is an object that responds to :path" do
+          let(:io_object) { EMFooIO.new }
+
+          before(:each) do
+            req
+              .stub(:body_stream)
+              .and_return(io_object)
+          end
+
           it "should set :file to object.path " do
-            my_io = EMFooIO.new
-            req.stub(:body_stream).and_return(my_io)
-            opts = handler.send(:fetch_request_options,(req))
-            opts[:file].should eql(my_io.path)
+            opts = handler.send(:fetch_request_options, req)
+            opts[:file].should eql(io_object.path)
           end
         end
-        
       end
 
       describe '#fetch_client_options' do
         it "should remove pool related options" do
           opts = handler.send(:fetch_client_options)
+
           opts.has_key?(:size).should be_false
           opts.has_key?(:never_block).should be_false
           opts.has_key?(:blocking_timeout).should be_false
         end
-        context "with_pool is true" do
+
+        context "when with_pool is true" do
+          before(:each) do
+            handler
+              .stub(:with_pool?)
+              .and_return(true)
+          end
+
           it "should set keepalive as true" do
-            handler.stub(:with_pool?).and_return(true)
             opts = handler.send(:fetch_client_options)
+
             opts[:keepalive].should be_true
           end
         end
-        context "with_pool is false" do
+
+        context "when with_pool is false" do
+          before(:each) do
+            handler
+              .stub(:with_pool?)
+              .and_return(false)
+          end
+
           it "should keepalive be false" do
-            handler.stub(:with_pool?).and_return(false)
             opts = handler.send(:fetch_client_options)
+
             opts[:keepalive].should_not be_true
           end
         end
-      end
-
-      it "should not timeout" do
-        EM.synchrony {
-          response = Http::Response.new
-          request = Http::Request.new
-          request.host = "127.0.0.1"
-          request.port = "8081"
-          request.uri = "/"
-          request.body_stream = StringIO.new("myStringIO")
-          # turn on our test server
-          EventMachine::run {
-            EventMachine::start_server request.host, request.port, SlowServer
-          }
-          handler.stub(:fetch_url).and_return("http://127.0.0.1:8081")
-          handler.handle(request,response)
-          response.network_error.should be_nil
-          EM.stop
-        }
-      end
-      it "should timeout after 0.1 seconds" do
-        EM.synchrony {
-          response = Http::Response.new
-          request = Http::Request.new
-          request.host = "127.0.0.1"
-          request.port = "8081"
-          request.uri = "/"
-          request.body_stream = StringIO.new("myStringIO")
-          # turn on our test server
-          EventMachine::run {
-            EventMachine::start_server request.host, request.port, SlowServer
-          }
-          handler.stub(:fetch_url).and_return("http://127.0.0.1:8081")
-          request.stub(:read_timeout).and_return(0.01)
-          handler.stub(:connect_timeout).and_return(1) #just to speed up the test
-          handler.handle(request,response)
-          response.network_error.should be_a(Timeout::Error)
-          EM.stop
-        }
       end
     end
   end
--- spec/patches_spec.rb
+++ spec/patches_spec.rb
@@ -7,20 +7,20 @@ describe Mutex do
       EM.stop
     end
   end
-  
+
   it "should be a fiber safe mutex" do
     EM.synchrony do
       AWS.mutex.new.should be_kind_of(EM::Synchrony::Thread::Mutex)
       EM.stop
     end
   end
-  
+
   it "should not affect Mutex outside AWS" do
     Mutex.new.should be_kind_of(Mutex)
   end
 end
 
-describe Kernel,'#sleep' do
+describe Kernel, '#sleep' do
   it "should be a fiber safe sleep from with AWS module" do
     EM.synchrony do
       EM::Synchrony.stub(:sleep).and_return("fiber safe")
@@ -28,7 +28,7 @@ describe Kernel,'#sleep' do
       EM.stop
     end
   end
-  
+
   it "should not affect normal Kernel.sleep if not in EM" do
     Kernel.sleep(1).should eql(1)
   end
